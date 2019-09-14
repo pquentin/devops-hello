dbf4ea78bc9aa16922d4eb6abd8c7b151cf46294
yen <yen.linkwang@nigao.re>
2019-09-14 10:33:42 +0400

    hello.py: 4rd try

--- hello.py
+++ hello.py
@@ -8,14 +8,13 @@ os.environ.setdefault("prometheus_multiproc_dir", "/tmp")
 app = Flask(__name__)
 metrics = GunicornPrometheusMetrics(app)
 
-
 @app.route("/")
 def index():
-    return "Hello Admin7, Admin6 is dead... A new World is coming! Will i have site unavailabe !"
+    return "Hello Admin7, Admin6 is dead..."
 
 
 def test_index():
     client = app.test_client()
     r = client.get("/")
-    assert r.data == b"Hello Admin7, Admin6 is dead... A new World is coming! Will i have site unavailabe !"
+    assert r.data == b"Hello Admin7, Admin6 is dead..."
     assert r.status_code == 200
