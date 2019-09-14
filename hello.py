e5685274ad314c4ca00b59331931ef9a1919f93c
tibo <tibo@localhost.localdomain>
2019-09-14 10:40:00 +0400

    hello change

--- hello.py
+++ hello.py
@@ -11,11 +11,11 @@ metrics = GunicornPrometheusMetrics(app)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return "Hello, Tib0 the world it's your!!"
 
 
 def test_index():
     client = app.test_client()
     r = client.get("/")
-    assert r.data == b"Hello, World!"
+    assert r.data == b"Hello, Tib0 the world it's your!!"
     assert r.status_code == 200
