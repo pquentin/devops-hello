23ae18e5256be9dfdcafae57338d3c11392fb466
Kevin CAZAL <kevin.cazal@epitech.eu>
2019-09-14 10:54:00 +0400

    More random char

--- hello.py
+++ hello.py
@@ -9,21 +9,20 @@ os.environ.setdefault("prometheus_multiproc_dir", "/tmp")
 app = Flask(__name__)
 metrics = GunicornPrometheusMetrics(app)
 
+choice = ["/", "\\", " ", " ", " ", "_"]
+
 
 @app.route("/")
 def index():
-    choice = ["/", "\\"]
     block = ""
     for i in range(80):
-        line = "".join([ choice[randint(0, 1)] for i in range(350)])
+        line = "".join([ choice[randint(0, 5)] for i in range(350)])
         block+=line+"<br>"
-    return block
-
+    return "<pre>" + block + "</pre>"
 
 
 def test_index():
     client = app.test_client()
     r = client.get("/")
-    assert r.data[0] == b"/" or b"\\"
-    assert r.data[-1] == b"/" or b"\\"
+    assert r.data
     assert r.status_code == 200
